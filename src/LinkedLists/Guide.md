4576fd6e290e59b73d2771789588b99946987974
Aryan Yadav <31789393+aryany9@users.noreply.github.com>
2024-12-21 12:06:58 +0530

    Added guide for LinkedList

---
+++ src/LinkedLists/Guide.md
@@ -0,0 +1,63 @@
+## What is Linked List?
+Linked List is a **linear data structure** where the elements are **not stored in contiguous locations**. 
+Every element is a **separate object** with a **data part** and **address part**.
+The elements are linked using **pointers** and **addresses** and each element is known as **NODE**.
+
+### EXAMPLE
+#### Java program to add elements to LinkedList
+```java
+import java.util.LinkedList;
+
+public class AddElements {
+    
+    // Main Method
+    public static void main(String[] args) {
+        // Creating a Linked List
+        LinkedList<String> ll = new LinkedList<String>();
+        
+        // Adding elements to the LinkedList using add() method
+        ll.add("One");
+        ll.add("Two");
+        ll.add("Three");
+        ll.add("Four");
+        ll.add("Five");
+        
+        // Printing the LinkedList
+        System.out.println(ll);
+    }
+}
+```
+#### Output
+```
+[One, Two, Three, Four, Five]
+```
+
+### Types of Linked List
+- **Singly Linked List**
+  - Each node **points to next** node, last node **points to null**.
+- **Doubly Linked List**
+  - Each **node has two pointers** p and n.
+  - p points to **previous node**, n points to the **next node**.
+  - **last** node's n points to **null**.
+- **Circular Linked List**
+  - Each node **points to the next node**, **last node** points back to **first node**.
+- **Circular-Doubly Linked List**
+  - Each node **points to the next node** (n) and **previous node** (p).
+  - **First** node's **(p)** points to the **last node**.
+  - **Last** node's **(n)** points to the **first node**.
+
+### Time Complexity
+- Accessing a Node: O(n)
+- Searching a Node: O(n)
+- Inserting a Node: O(1)
+- Removing a Node: O(1)
+
+### How Does LinkedList work internally?
+Since a LinkedList acts as a **dynamic array** where you **do not have to specify the size** while creating it. The size of the list **automatically increases** when we dynamically **add** or **remove** items. Internally the LinkedList is implemented using the **Doubly Linked List** data structure.
+
+
+### References
+```
+https://www.geeksforgeeks.org/linked-list-java/
+https://leetcode.com/discuss/study-guide/1800120/Become-Master-In-Linked-List
+```
--- src/LinkedLists/LinkedListCreation.java
+++ src/LinkedLists/LinkedListCreation.java
@@ -1,5 +1,7 @@
 package LinkedLists;
 
+import java.util.LinkedList;
+
 public class LinkedListCreation {
 
     // CREATION of NODE
@@ -11,17 +13,34 @@ public class LinkedListCreation {
             this.data = data;
             this.next = null;
         }
-
     }
 
     // Initially both head and tail are not
     // pointing to any other node
-    Node head = null;
-    Node tail = null;
+    static Node head = null;
+    static Node tail = null;
 
+    // Method to add New Node to Linked List
+    static void addNode(int data){
+        Node newNode = new Node(data);
+
+        // Checks if the list is empty
+        if(head == null){
+            /*
+            * If the list is empty, both head
+            * and tail will point to new node
+            * */
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            // Storing new node in tail
+            tail = newNode;
+        }
+    }
 
     // Method to Count Nodes
-    int countNodes(){
+    static int countNodes(){
         // Initially Zero
         int count = 0;
 
@@ -37,11 +56,38 @@ public class LinkedListCreation {
         return count;
     }
 
+    static void displayNodes(){
+        Node currentNode = head;
+
+        if(head == null){
+            System.out.println("Empty");
+            return;
+        }
+        System.out.println("Nodes: ");
+        while(currentNode != null){
+            System.out.println(currentNode.data + " ");
+            currentNode = currentNode.next;
+        };
+    }
+
     public static void main(String[] args) {
-        Node node = new Node(10);
-        node.next = new Node(20);
-        System.out.println(node.data);
-        System.out.println(node.next.data);
+        addNode(1);
+        addNode(2);
+        displayNodes();
+        System.out.println("Total Nodes: "+countNodes());
+
+        // Creating a Linked List
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // Adding elements to the LinkedList using add() method
+        ll.add("One");
+        ll.add("Two");
+        ll.add("Three");
+        ll.add("Four");
+        ll.add("Five");
+
+        // Printing the LinkedList
+        System.out.println(ll);
     }
 
 }
