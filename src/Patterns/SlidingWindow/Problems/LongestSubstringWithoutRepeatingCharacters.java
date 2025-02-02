d462367e4a04693baef077f10fe3c009354ecea5
aryan <aryanyadav9.2000@gmail.com>
2025-02-02 08:46:18 +0530

    Added Two Pointer Approach and Brute Force Approch

---
+++ src/Patterns/SlidingWindow/Problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,82 @@
+package Patterns.SlidingWindow.Problems;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    static void bruteForceApproach(String s){
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                if(set.contains(s.charAt(j))){
+                    break;
+                }
+                int length = j-i+1;
+                maxLength = Math.max(length, maxLength);
+                set.add(s.charAt(j));
+            }
+        }
+        System.out.println("Max Length: "+maxLength);
+    }
+
+    static void twoPointerApproach1(String s) {
+        int left = 0;
+
+        if(s.isEmpty()){
+            return;
+        }
+
+        int maxLength = Integer.MIN_VALUE;
+        Set<Character> set = new HashSet<>();
+
+        // Outer loop for traversing the string.
+        for (int right = 0; right < s.length(); right++) {
+            if (set.contains(s.charAt(right))){
+                // If duplicate element is found
+                while (left < right && set.contains(s.charAt(right))){
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right-left+1);
+        }
+        System.out.println("Max Length: "+maxLength);
+    }
+
+
+    static void twoPointerApproach2(String s){
+        int maxLength = 0;
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        int left = 0, right = 0;
+        int n = s.length();
+
+        while (right < n){
+            if (map.containsKey(s.charAt(right))) left = Math.max(map.get(s.charAt(right)) + 1, left);
+            map.put(s.charAt(right), right);
+
+            maxLength = Math.max(maxLength, right-left+1);
+            right++;
+        }
+
+        System.out.println("Max Length: "+maxLength);
+    }
+
+    public static void main(String[] args) {
+        String i = "cadbzabcd";
+
+        // Brute Force Approach
+        bruteForceApproach(i);
+
+        // Two Pointer Approach 1
+        twoPointerApproach1(i);
+
+        // Two Pointer Approach 2
+        twoPointerApproach2(i);
+
+    }
+}
