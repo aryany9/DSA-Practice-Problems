70369aa8512c2ad7c8533032f90e74a50365027f
aryan <aryanyadav9.2000@gmail.com>
2025-03-18 00:27:08 +0530

    Added new approach and two pointer approach for three sum

--- src/DataStructures/Arrays/Problems/ThreeSum.java
+++ src/DataStructures/Arrays/Problems/ThreeSum.java
@@ -3,7 +3,7 @@ package DataStructures.Arrays.Problems;
 import java.util.*;
 
 public class ThreeSum {
-    public static void threeSum(int[] nums) {
+    public static void bruteForceApproach(int[] nums) {
         int n = nums.length;
         Set<List<Integer>> s = new HashSet<>();
 
@@ -27,8 +27,83 @@ public class ThreeSum {
         System.out.println(s);
     }
 
+
+    public static void approachOne(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+
+        // a + b + c = 0;
+        // b + c = -a
+        // c = -a - b;
+
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            Set<Integer> s = new HashSet<>();
+
+            for (int j = i+1; j < nums.length; j++) {
+                int b = nums[j];
+                int c = -a - b;
+
+                if(s.contains(c)){
+                    List<Integer> triplet = Arrays.asList(a, b, c);
+
+                    // Sorting to keep it unique before storing
+                    Collections.sort(triplet);
+
+                    result.add(triplet);
+                }
+                s.add(b);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void betterApproach(int[] nums) {
+        int n = nums.length;
+        Set<List<Integer>> result = new HashSet<>();
+
+        // Sorting the array first
+        Arrays.sort(nums);
+
+        // Iterating the Loop
+        for (int i = 0; i < n; i++) {
+
+            // skipping i if already has the same value
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i+1;
+            int k = n-1;
+
+            int target = 0;
+
+            while(j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum < target){
+                    j++;
+                } else if (sum > target){
+                    k--;
+                } else {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    result.add(triplet);
+
+                    // Iterating the  pointers
+                    j++;
+                    k--;
+
+                    // Skipping j if already visited with same value
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         int[] input = {-1,0,1,2,-1,-4};
-        threeSum(input);
+        bruteForceApproach(input);
+        approachOne(input);
+        betterApproach(input);
     }
 }
